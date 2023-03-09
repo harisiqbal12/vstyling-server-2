460d6a1566403074a0aead8ef3124e17a4ce95d4
HarrisK <haris6072@gmail.com>
2023-03-09 19:00:54 +0500

    init

---
+++ dist/server.js
@@ -0,0 +1,42 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const http_1 = __importDefault(require("http"));
+const socket_io_1 = require("socket.io");
+const app = (0, express_1.default)();
+const server = http_1.default.createServer(app);
+const io = new socket_io_1.Server(server);
+app.use((0, cors_1.default)());
+app.get('/', (req, res) => {
+    res.status(200).send('<h2>Explore Creation Socket Server</h2>');
+});
+io.on('connection', socket => {
+    console.log('A client has connected');
+    socket.emit('connected-client', true);
+    socket.on('notifyOrders', data => {
+        console.log('new order occures');
+        console.log('data: ', data);
+        io.emit('incomingNeworder', data);
+        console.log('emitted');
+    });
+    socket.on('secondApp', data => {
+        console.log('second app connected');
+    });
+    socket.on('notifyUsers', data => {
+        console.log('incomning new user');
+        console.log('data: ', data);
+        io.emit('incominguser', data);
+        console.log('emited');
+    });
+    socket.on('disconnect', function () {
+        console.log('Socket disconnected');
+    });
+});
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => {
+    console.log('socket server running on: ', PORT);
+});
---
+++ server.ts
@@ -0,0 +1,48 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import http from 'http';
+import { Server } from 'socket.io';
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+	res.status(200).send('<h2>Explore Creation Socket Server</h2>');
+});
+
+io.on('connection', socket => {
+	console.log('A client has connected');
+
+	socket.emit('connected-client', true);
+
+	socket.on('notifyOrders', data => {
+		console.log('new order occures');
+		console.log('data: ', data);
+		io.emit('incomingNeworder', data);
+		console.log('emitted');
+	});
+
+	socket.on('secondApp', data => {
+		console.log('second app connected');
+	});
+
+	socket.on('notifyUsers', data => {
+		console.log('incomning new user');
+		console.log('data: ', data);
+		io.emit('incominguser', data);
+		console.log('emited');
+	});
+
+	socket.on('disconnect', function () {
+		console.log('Socket disconnected');
+	});
+});
+
+const PORT = process.env.PORT || 4000;
+
+server.listen(PORT, () => {
+	console.log('socket server running on: ', PORT);
+});
